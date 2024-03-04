48bd1183769ad5dec95b7b3b35d799ee4852ac2b
Edjek <edjek@hotmail.fr>
2024-03-04 16:08:44 +0100

    first day

---
+++ assets/js/07-boucles.js
@@ -0,0 +1,12 @@
+/* --------------------------- BOUCLES --------------------------- */
+
+// Les boucles sont des structures permettant d'executer plusieurs fois des instructions
+
+// Écrire un programme qui demande à l'utilisateur de saisir un nombre entier compris entre 1 et 10.
+// Si la réponse n'est pas entre 1 et 10, afficher que le message devait être compris entre 1 et 10
+// Le programme doit ensuite dessiner un sapin de Noël composé d'étoiles (*)
+// *
+// **
+// ***
+// ****
+// *****
