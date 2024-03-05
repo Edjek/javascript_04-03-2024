d534bab4d38f269ae0637e4d06f505a2d011bb6a
Edjek <edjek@hotmail.fr>
2024-03-05 21:19:40 +0100

    second day

--- assets/js/09-tableaux.js
+++ assets/js/09-tableaux.js
@@ -5,10 +5,37 @@
     On peut créer un tableau en utlisant des crochets [] et en y insérants les éléments séparés par des virgules
 */
 
+// Déclaration d'un tableau vide
 const tableau = [];
 
-// Créer un tableau avec tous les mois de l'année
-// faire un console.log du tableau
-// Afficher en console janvier et decembre
-// Afficher tous les mois de l'année avec une boucle
-// transformer cette boucle en fonction sans parametre
+const monthOfYear = [
+    'janvier',
+    'fevrier',
+    'mars',
+    'avril',
+    'mai',
+    'juin',
+    'juillet',
+    'aout',
+    'septembre',
+    'octobre',
+    'novembre',
+    'decembre',
+];
+
+console.log(monthOfYear);
+
+// On peut accéder à un élément du tableau en utilisant son index numérique.
+// L'index du premier élément est toujours 0.
+console.log(monthOfYear[0]);
+console.log(monthOfYear[2]);
+
+// Pour connaitre la longueur d'un element nous pouvons utiliser la propriété .length
+console.log(monthOfYear.length);
+
+function displayMonthOfYear() {
+    for (let i = 0; i < monthOfYear.length; i++) {
+        console.log(monthOfYear[i]);
+    }
+}
+displayMonthOfYear();
