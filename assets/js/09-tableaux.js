afa8d814f0c4bf2a1780991c7c4e7502848995d5
Edjek <edjek@hotmail.fr>
2024-03-05 15:15:24 +0100

    day 2

---
+++ assets/js/09-tableaux.js
@@ -0,0 +1,14 @@
+/* --------------------------- TABLEAUX --------------------------- */
+
+/*
+    Un tableau (array) est un type de donnée qui peut contenir plusieurs éléments
+    On peut créer un tableau en utlisant des crochets [] et en y insérants les éléments séparés par des virgules
+*/
+
+const tableau = [];
+
+// Créer un tableau avec tous les mois de l'année
+// faire un console.log du tableau
+// Afficher en console janvier et decembre
+// Afficher tous les mois de l'année avec une boucle
+// transformer cette boucle en fonction sans parametre
