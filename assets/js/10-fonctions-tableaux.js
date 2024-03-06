18e4eb9d3a9c8c328fc8e61b9c9b733c5b1cdb47
Edjek <edjek@hotmail.fr>
2024-03-06 14:00:26 +0100

    third day

--- assets/js/10-fonctions-tableaux.js
+++ assets/js/10-fonctions-tableaux.js
@@ -8,43 +8,66 @@ const animals = ['chat', 'chien', 'tigre', 'lion', 'requin', 'panda'];
 // Ajouter un ou plusieurs éléments à la fin d'un tableau
 // Renvoie la nouvelle longueur du tableau
 let response = animals.push('dauphin', 'loup');
-console.log(response);
+// console.log(response);
 
 // Supprimer le dernier élément du tableau
 // Renvoie l'élément supprimé
 response = animals.pop();
-console.log(response);
+// console.log(response);
 
 // Ajouter un ou plusieurs éléments au début du tableau
 // Renvoie la nouvelle longueur du tableau
 response = animals.unshift('hiboux');
-console.log(response);
+// console.log(response);
 
 // Supprimer le premier élément du tableau
 // Renvoie l'élément supprimé
 response = animals.shift();
-console.log(response);
+// console.log(response);
 
 // Réorganiser le tableau de la fin vers le début
 animals.reverse();
-console.log(animals);
+// console.log(animals);
 
 // Modifier ou Ajouter ou Supprimer un élément dans un tableau
 // Ajouter
 animals.splice(1, 0, 'raton laveur');
-console.log(animals);
+// console.log(animals);
 // Modifier
 animals.splice(6, 1, 'aigle');
-console.log(animals);
+// console.log(animals);
 // Supprimer
 animals.splice(2, 1);
-console.log(animals);
+// console.log(animals);
 
-// Declarer un tableau contenant (Paris, Berlin, Rome, Moscou, Londres, Madrid)
-/*
-    Créer une fonction qui prend en parametre un tableau:
-        Retire Madrid
-        Ajoute au debut Vienne
-        Rome devient Alger
-        Retourne la taille du tableau
-*/
+for (let i = 0; i < animals.length; i++) {
+    // console.log(animals[i]);
+}
+
+// for(.. of ..){} permet de parcourir les elements d'un objet itérable (ex: tableau, string)
+for (let animal of animals) {
+    // console.log(animal);
+}
+
+// foreach(){} est une méthode Javascript qui permet d'itérer sur les éléments d'un tableau
+animals.forEach(function (animal) {
+    console.log(animal);
+});
+
+const capitalCities = [
+    'Paris',
+    'Berlin',
+    'Rome',
+    'Moscou',
+    'Londres',
+    'Madrid',
+];
+
+const modifyArray = (array) => {
+    // array.pop()
+    // array.unshift('Vienne')
+    // array.splice(3, 1, 'Alger')
+    // return array.length
+};
+
+const capitalCitiesLength = modifyArray(capitalCities);
---
+++ assets/js/11-callback.js
@@ -0,0 +1,73 @@
+/* --------------------------- CALLBACK --------------------------- */
+
+// Un callback (fonction de rappel) est une fonction qui est passée en tant qu'argument dans une autre fonction.
+
+// function add
+const add = function (x, y) {
+    return x + y;
+};
+
+// function divide
+const divide = function (x, y) {
+    return x / y;
+};
+
+// function multiply
+const multiply = function (x, y) {
+    return x * y;
+};
+
+function mathematique(x, y, callback) {
+    return callback(x, y);
+}
+
+const res = mathematique(12, 89, function (a, t) {
+    return a * 5 + t * 9;
+});
+console.log(res);
+
+// Une fonction peut prendre en parametre une autre fonction
+// cela permet de rendre personnalisable
+function display(callback) {
+    callback();
+}
+
+const hello = function () {
+    console.log('salut tout le monde');
+};
+
+display(hello);
+
+display(function () {
+    console.log(23 + 67);
+});
+
+// Recreons la fonction forEach()
+const monthOfYear = [
+    'janvier',
+    'fevrier',
+    'mars',
+    'avril',
+    'mai',
+    'juin',
+    'juillet',
+    'aout',
+    'septembre',
+    'octobre',
+    'novembre',
+    'decembre',
+];
+
+const displayArray = function (string) {
+    console.log(string);
+};
+
+function forEachPerso(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        callback(array[i]);
+    }
+}
+
+forEachPerso(monthOfYear, function () {
+    console.log(' 2024');
+});
