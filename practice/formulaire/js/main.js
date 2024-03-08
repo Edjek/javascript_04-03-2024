8c2f1762442fa3b394998a5f861d04b60821bc0e
Edjek <edjek@hotmail.fr>
2024-03-08 16:55:35 +0100

    fifth day

--- practice/formulaire/js/main.js
+++ practice/formulaire/js/main.js
@@ -1,1 +1,21 @@
-console.log('object');
+const submit = document.querySelector('#submit');
+
+submit.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const nameInput = document.querySelector('#name');
+    const emailInput = document.querySelector('#email');
+
+    if (nameInput.value == '') {
+        const messageError = document.createElement('p');
+        messageError.textContent = 'Le champs est vide';
+        messageError.classList.add('error-message');
+        console.log(nameInput.parentElement);
+        nameInput.parentElement.append(messageError);
+    } else if (emailInput.value == '') {
+        const messageError = document.createElement('p');
+        messageError.textContent = 'Le champs est vide';
+        messageError.classList.add('error-message');
+        emailInput.parentElement.append(messageError);
+    }
+});
